99b70b32d61ed5babf909d7a8b716e6d5414eede
Abhay Choudhary <abhayychoudhary@gmail.com>
2021-01-27 23:38:31 +0530

    change in main file

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ import json
 import trainingjsonfile
 
 # Define The folder name
-csvfile = "TrainingCSV/Templet.csv"
+csvfile = "Templet1.csv"
 
 # Define the Folder name where you want to create all the JSON file.
-importFolder="Intent/"
+importFolder=""
 
 with open(csvfile, 'r') as file:
     reader = csv.reader(file)
     next(reader, None)
     for row in reader:
-        trainingjsonfile.userSays(row)
-        print(trainingjsonfile.userSays(row))
+        # trainingjsonfile.userSays(row)
+        # print(trainingjsonfile.userSays(row))
         
-        if row[21]:
+        if row[5]:
             with open(importFolder+row[0]+".json",'w') as f:
                 json.dump(trainingjsonfile.defaultcontext(row), f, indent=2, ensure_ascii=False)
         else:
-            with open(importFolder+row[0]+"_usersays_hi.json", 'w') as f:
-                json.dump(trainingjsonfile.userSays(row), f, indent=2, ensure_ascii=False)
+            if(row[10]):
+                with open(importFolder+row[0]+"_usersays_+"row[10]"+.json", 'w') as f:
+                    json.dump(trainingjsonfile.userSays(row), f, indent=2, ensure_ascii=False)
+            else:
+                with open(importFolder+row[0]+"_usersays_en.json", 'w') as f:
+                    json.dump(trainingjsonfile.userSays(row), f, indent=2, ensure_ascii=False)
+                
             if row[3] and row[4] or row[3]:
                 trainingjsonfile.outputOutputContext(row)
                 print(trainingjsonfile.outputOutputContext(row))
